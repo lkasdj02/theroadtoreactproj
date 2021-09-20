2dbf3847bc8790354122ed5fb11610fbcbdecc67
Zeo <leonardo.basso02@gmail.com>
2021-09-20 11:36:26 +0200

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+const arr = [{ name: "leonardo" }, { name: "anita" }];
+const name = "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {arr.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
