c9a3e4d2ad9ae1cabf051c2b3bcea024bf126525
Zeo <leonardo.basso02@gmail.com>
2021-09-20 15:39:37 +0200

    finished with props chapter

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 const name = "react";
 
-function List() {
-  let myList = [
-    ...list,
-    {
-      title: "Other",
-      url: "https://other.org/",
-      author: "me",
-      num_comments: 0,
-      points: 5,
-      objectID: 2,
-    },
-  ];
-  console.log(list);
-  console.log(myList);
+function List(props) {
+  // let myList = [
+  //   ...list,
+  //   {
+  //     title: "Other",
+  //     url: "https://other.org/",
+  //     author: "me",
+  //     num_comments: 0,
+  //     points: 5,
+  //     objectID: 2,
+  //   },
+  // ];
   return (
     <ul>
-      {myList.map((item) => (
-        <li key={item?.objectID}>
-          <span>{item?.title}</span>
-          <span>{item?.author}</span>
-          <span>{item?.points}</span>
-          <span>{item?.num_comments}</span>
-        </li> // DO NOT USE THE INDEX OF THE MAP FUNCTION FOR THE KEY ATTRIBUTE.
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} /> // DO NOT USE THE INDEX OF THE MAP FUNCTION FOR THE KEY ATTRIBUTE.
       ))}
     </ul>
   );
 }
+
+const Item = ({ item }) => (
+  <ul>
+    <li key={item?.objectID}>
+      <span>{item?.title}</span> <br />
+      <span>{item?.author}</span> <br />
+      <span>{item?.points}</span> <br />
+      <span>{item?.num_comments}</span>
+    </li>
+  </ul>
+);
 
 const Search = () => {
   // do something in between
@@ -63,19 +48,37 @@ const Search = () => {
   );
 };
 
-function App() {
+const App = () => {
   // let otherList = list.filter((item) => item.objectID !== 1); THESE ARE JUST CURIOSITIES
   // console.log(list);
   // console.log(otherList);
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div className="App">
       <h1>My hacker stories</h1>
       <Search />
       <hr />
-      <List />
-      <List />
+      <List list={stories} />
     </div>
   );
-}
+};
 
 export default App;
